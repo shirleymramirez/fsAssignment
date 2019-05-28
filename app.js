54a6c492b9b714171ef38a11da4bdb0ce8f0f840
shirley <shirley.mramirez@yahoo.com>
2019-05-28 12:33:08 -0700

    done with challenge1

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 var fs = require('fs');
+
+let challenges1 = fs.readFileSync('./challenge1/info.txt', 'utf8', (err, data) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+console.log(challenges1);
