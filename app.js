4f94c4101c617c67b8548454737edad00c8b0f50
shirley <shirley.mramirez@yahoo.com>
2019-05-28 13:32:22 -0700

    done with challenge2

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ let challenges1 = fs.readFileSync('./challenge1/info.txt', 'utf8', (err, data) =
 });
 
 console.log(challenges1);
+
+let groupName = fs.readFileSync('challenge2/info.txt', 'utf-8', (err, data) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+groupName = groupName + '\nShirley\nGladys\nGlai\nShiela'
+fs.writeFileSync('challenge2/info.txt', groupName)
+
+
