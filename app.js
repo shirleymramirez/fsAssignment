1929cc20790a529dbbd5eda34acfb643bead50ba
shirley <shirley.mramirez@yahoo.com>
2019-05-28 13:35:57 -0700

    done with challenge3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 
+// ## Challenge 1:
+// Simply print the contents of the text file to the screen.
 let challenges1 = fs.readFileSync('./challenge1/info.txt', 'utf8', (err, data) => {
     if(err) {
         console.log(err);
@@ -10,6 +12,8 @@ let challenges1 = fs.readFileSync('./challenge1/info.txt', 'utf8', (err, data) =
 
 console.log(challenges1);
 
+// ## Challenge 2:
+// Add the names of everyone in your group to the end of the text file.
 let groupName = fs.readFileSync('challenge2/info.txt', 'utf-8', (err, data) => {
     if(err) {
         console.log(err);
@@ -21,4 +25,7 @@ let groupName = fs.readFileSync('challenge2/info.txt', 'utf-8', (err, data) => {
 groupName = groupName + '\nShirley\nGladys\nGlai\nShiela'
 fs.writeFileSync('challenge2/info.txt', groupName)
 
+// ## Challenge 3:
+// The file was incorrectly named binfo.txt ...Rename it to info.txt(only using js!)
+fs.renameSync("./challenge3/binfo.txt", "./challenge3/info.txt")
 
